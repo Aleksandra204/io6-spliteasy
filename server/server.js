da5776ddaa6491ea49df4eac109c8d7996aa9ee9
marcin <marcindendys@onet.eu>
2025-05-26 13:09:15 +0200

    DEV: Dodawanie grupy - Backend

--- server/server.js
+++ server/server.js
@@ -69,4 +69,92 @@ app.post("/login", async (req, res) => {
   }
 });
 
+
 app.listen(port, () => console.log(`🚀 Serwer działa na http://localhost:${port}`));
+
+
+
+
+
+function verifyToken(req, res, next) {
+  const cookies = req.headers.cookie;
+  if (!cookies) return res.status(401).json({ msg: "Brak ciasteczek" });
+
+  const parsedCookies = cookie.parse(cookies);
+  const token = parsedCookies.jwtToken;
+
+  if (!token) return res.status(401).json({ msg: "Brak tokenu" });
+
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if (err) return res.status(403).json({ msg: "Nieprawidłowy token" });
+    req.user = decoded;
+    next();
+  });
+}
+
+
+
+app.get("/user/:email", verifyToken, async (req, res) => {
+  const { email } = req.params;
+  try {
+    const result = await pool.query('SELECT id, name FROM "User" WHERE mail = $1', [email]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ msg: "Użytkownik nie znaleziony" });
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Błąd serwera" });
+  }
+});
+
+
+app.post("/group", verifyToken, async (req, res) => {
+  const { name, userIds } = req.body;
+  const ownerId = req.user.userId;
+
+  // console.log("req.user:", req.user);
+  // console.log("ownerId:", ownerId);
+  // console.log("name:", name);
+
+  if (!name || !Array.isArray(userIds) || userIds.length === 0) {
+    return res.status(400).json({ msg: "Nieprawidłowe dane" });
+  }
+
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+
+    // Utworzenie grupy
+    const groupRes = await client.query(
+      'INSERT INTO "Group" (name, owner_user_id) VALUES ($1, $2) RETURNING id',
+      [name, ownerId]
+    );
+
+    const groupId = groupRes.rows[0].id;
+
+    // Dodanie osób i  właściciela do grupy
+    const allUserIds = Array.from(new Set([...userIds, ownerId]));
+
+    const insertPromises = allUserIds.map((userId) =>
+      client.query(
+        'INSERT INTO Group_users (group_id, user_id) VALUES ($1, $2)',
+        [groupId, userId]
+      )
+    );
+
+    await Promise.all(insertPromises);
+    await client.query("COMMIT");
+
+    res.status(201).json({ msg: "Grupa utworzona", groupId });
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error(err);
+    res.status(500).json({ msg: "Błąd tworzenia grupy" });
+  } finally {
+    client.release();
+  }
+});
+
+
+
